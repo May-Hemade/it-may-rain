e01968181c694c080f7b159dc0a26656f1d0955a
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2022-03-11 13:39:33 +0200

    initial setup

---
+++ src/components/HeaderBar.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { Button, Form, FormControl, Nav, Navbar } from "react-bootstrap"
+import { useForm } from "react-hook-form"
+import { Link } from "react-router-dom"
+
+import { useNavigate } from "react-router-dom"
+
+export default function Headbar() {
+  const { register, handleSubmit } = useForm()
+  const navigate = useNavigate()
+  const onSubmit = (data) => {
+    navigate(`/search/${data.search}`)
+  }
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">It MayRain</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/favorites">
+              <div className="nav-link">Today</div>
+            </Link>
+            <Link to="/favorites">
+              <div className="nav-link">Daily</div>
+            </Link>
+          </Nav>
+          <Form onSubmit={handleSubmit(onSubmit)} inline>
+            <FormControl
+              type="text"
+              input
+              {...register("search", { required: true })}
+              placeholder="Search"
+              className="mr-sm-2"
+            />
+            <Button type="submit" variant="outline-success">
+              Search
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  )
+}
---
+++ src/redux/reducers/homeReducer.js
@@ -0,0 +1,8 @@
+const homeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default homeReducer
